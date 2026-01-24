4f7940b8a07e0074ed9141665da9c881bde76c28
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2026-01-24 21:30:00 +0530

    chore: secure local configuration and ignore environment files

--- bidstream-backend/run.sh
+++ bidstream-backend/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Load environment variables from the root .env file and run Spring Boot
-export $(grep -v '^#' ../../.env | xargs)
+export $(grep -v '^#' ../.env | xargs)
 ./mvnw spring-boot:run
